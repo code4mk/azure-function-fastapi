9d962976114df0f391cd790fdd6e179dfd528317
Mostafa Kamal <hiremostafa@gmail.com>
2024-05-19 17:23:32 +0600

    schema and serializer update

--- fastapi_project/schemas/user_schema.py
+++ fastapi_project/schemas/user_schema.py
@@ -3,15 +3,13 @@ from typing import Optional
 
 # Pydantic model for creating a new user
 class UserCreateSchema(BaseModel):
-    full_name: str
+    name: str
     email: EmailStr
     password: str
 
 
 # Pydantic model for updating user data
 class UserUpdateSchema(BaseModel):
-    full_name: Optional[str] = None
+    name: Optional[str] = None
     email: Optional[EmailStr] = None
     password: Optional[str] = None
-    is_active: Optional[bool] = None
-    is_superuser: Optional[bool] = None
--- fastapi_project/serializers/user_serializer.py
+++ fastapi_project/serializers/user_serializer.py
@@ -2,10 +2,8 @@ from pydantic import BaseModel, EmailStr
 
 class UserSerializer(BaseModel):
     id: int
-    full_name: str
+    name: str
     email: EmailStr
-    is_active: bool
-    is_superuser: bool
 
     class Config:
         from_orm = True
